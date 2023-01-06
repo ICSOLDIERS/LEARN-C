2fc460fd077efd2270f64e584b33545c001bb6a7
ABHISHEK SINGH <81334208+ICSOLDIERS@users.noreply.github.com>
2023-01-06 21:29:25 +0530

    this is first

---
+++ cprogramming basics/first..c
@@ -0,0 +1,89 @@
+# include<stdio.h>
+
+int main(){
+    printf("helllo i am learning c programming");
+    return 0;
+}
+// #############################################################################################################################################
+// Variables:--- a variables is a container which stores a value. In kitchen we have containers stroring rice ,dal ,sugar etc.
+// similar to that variables in c stores values of a constant 
+
+// a = 3 ;   a is assinged 3     "semicolon is instruction terminator"
+// b = 4.7;  b is assigned 4.7
+// c = "A";  c is storing "A"
+
+// RULES FOR NAMING IN VARIABLES:
+// 1.FIRST CHARECTER MUST ME ALPHABET OR UNDERSCORE
+// 2.NO COMMAS ,BLANKS ALLOWED
+// 3.NO SPECIAL SYMBOL OTHER THAN UNDERSCORE
+// 4.VARIABLE NAME IS CASE-SENSITIVE
+// WE MUST CREATE MEANINGFULL VARIABLES.THIS ENHANCE READABILITYOF OUR PROGRAM
+// #############################################################################################################################################
+
+// CONSTANTS:--An entity whose doesnot cahnge is called constants.a variable is an entity which value can be changed.
+
+// type of constants:
+// 1.integer constants: -1,6,4,7,9
+// 2.real constants :- -327.1,2.5.7.0
+// 3.charecter constants: 'a','#','@'  must be written in single inverted comma
+
+// ##############################################################################################################################################
+
+// KEYWORDS: these are reserved words whose meaning already known to compiler .there are 32 keywords availabe in c.
+// auto break case char const continue default do double long return register short signed sizeof static int else enum extern float for goto if 
+// struct typedef union unsigned void volatile while
+
+// our first c program :
+
+// lok at above
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// BASIC STRUCTURE OF A C PROGRAM :all c program have to fllow a basic structure . a c program starts with a main function and execute 
+// instructions present inside it .
+// each instruction is terminated with a semicolon(;)
+// there are some rules which are applicable to all cprogram:
+// 1.very program excution must start with main function.
+// 2.all the statements are terminated by semicolon.
+// 3.instructions are case-sensitive.
+// 4.instructions are execute in the same order in which they are written.
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// COMMENTS IN C:
+// 1:SINGLE LINE COMMENT :   // 
+// 2.MULTILINE COMMENT:  /*    DKSFN  */
+
+// #############################################################################################################################################
+
+
+/* THIS IS OUR FIRST C PROGRAM THATS IT!*/
+// COMMENTS ARE JUST PRASED NOT EXECUTED.
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// COMPILATION AND EXECUTION:a compiler is a computer program which converts a c program into a machine lang so it can be easily understand by the computer.
+// file.c---->c compiler ------> file.exe this is the process .
+
+
+/* a c program is written in plain texts .this plain text is combination of instructions in a particular sequence .the compiler performs some basic 
+checks and finallly converts the program into an executable*/
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// LIBRARY FUNCTIONS IN C: 
+/* c language has a lot of valuab;le library functions which is used to carry out certain tasks .for instance printf function is used to
+print the value on the screen.
+
+*/
+/* 
+printf("this is %d",i)
+%d for integers
+%f for reals
+%c for charecters
+*/
+
+
---
+++ cprogramming basics/variables.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main(){
+    int a=4;
+    // int b=4.7;  //this is not recommended
+    float b= 4.7;
+    char c='A';
+    int e =49 ;
+    printf("the value of a is %d \n",a);
+    printf("the value of b is %f \n",b);
+    printf("the value of c is %c\n",c);
+    printf("the value of a and e is %d",a + e);
+    return 0;
+
+
+}
